3c1077218e4391b393421cbd72297e51cddc0a72
swathi <swathisherigar2003@gmail.com>
2025-01-01 23:44:27 +0530

    Commit

--- academy/urls.py
+++ academy/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('select-year-semester/<slug:branch_slug>/', views.select_year_semester, name='select_year_semester'),
     path('videos_notes/<slug:branch_slug>/', views.videos_notes, name='videos_notes'),
-]
+]+ static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- vtu_academy/urls.py
+++ vtu_academy/urls.py
@@ -18,6 +18,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+from vtu_academy.vtu_academy import settings
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', include('academy.urls')), 
