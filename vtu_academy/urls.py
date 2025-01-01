da6c7f0ffcd5944b24a098707a4d669423425c47
swathi <swathisherigar2003@gmail.com>
2025-01-01 23:53:34 +0530

    commit

--- vtu_academy/urls.py
+++ vtu_academy/urls.py
@@ -18,7 +18,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
-from vtu_academy.vtu_academy import settings
 
 urlpatterns = [
     path("admin/", admin.site.urls),
